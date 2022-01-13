0ed630e708b3e848ad3eb7f5f2276df37628e083
Daniel Hott <daniel_hott@outlook.com>
2022-01-13 15:42:12 -0300

    Updates

---
+++ precache-manifest.82c12f156d5eb23bb520aa7a73e9ddf1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616047790100e39eaab59823ee76d5b4",
+    "url": "/DanielHott/AppFood/index.html"
+  },
+  {
+    "revision": "b1b1a6302a261222462f",
+    "url": "/DanielHott/AppFood/static/css/2.389ceec1.chunk.css"
+  },
+  {
+    "revision": "a3ac61346768627a27ea",
+    "url": "/DanielHott/AppFood/static/css/main.0c1ae31f.chunk.css"
+  },
+  {
+    "revision": "b1b1a6302a261222462f",
+    "url": "/DanielHott/AppFood/static/js/2.09be1839.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/DanielHott/AppFood/static/js/2.09be1839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ac61346768627a27ea",
+    "url": "/DanielHott/AppFood/static/js/main.e1103005.chunk.js"
+  },
+  {
+    "revision": "a73dd0aa5f1f200d20c1",
+    "url": "/DanielHott/AppFood/static/js/runtime-main.22989b55.js"
+  },
+  {
+    "revision": "30e3e10bfc3dcee165c61cc84eedb4eb",
+    "url": "/DanielHott/AppFood/static/media/beer.30e3e10b.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/DanielHott/AppFood/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "8eac531f3cd6dc1014ddc1a7fe6a7fe8",
+    "url": "/DanielHott/AppFood/static/media/bussola.8eac531f.png"
+  },
+  {
+    "revision": "37f84db3dd869c499781b7a14e306401",
+    "url": "/DanielHott/AppFood/static/media/heart.37f84db3.png"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/DanielHott/AppFood/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "785b4453f035641b0206ad581c0a642f",
+    "url": "/DanielHott/AppFood/static/media/restaurant.785b4453.png"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/DanielHott/AppFood/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/DanielHott/AppFood/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
