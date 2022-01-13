04069007cb17d0c68889ca01c05070866c506b66
Daniel Hott <daniel_hott@outlook.com>
2022-01-13 16:29:11 -0300

    Updates

---
+++ precache-manifest.88c6ee60f868aab1742e65bb113adad3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb83b39a3433fd3b66c39648f35efaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75f7e9a7231e89968464",
+    "url": "/static/css/2.389ceec1.chunk.css"
+  },
+  {
+    "revision": "27e0a9b3ec93e408301e",
+    "url": "/static/css/main.0c1ae31f.chunk.css"
+  },
+  {
+    "revision": "75f7e9a7231e89968464",
+    "url": "/static/js/2.5d684dfe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5d684dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e0a9b3ec93e408301e",
+    "url": "/static/js/main.8856f10a.chunk.js"
+  },
+  {
+    "revision": "82508395ca765ce76498",
+    "url": "/static/js/runtime-main.c2683745.js"
+  },
+  {
+    "revision": "30e3e10bfc3dcee165c61cc84eedb4eb",
+    "url": "/static/media/beer.30e3e10b.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "8eac531f3cd6dc1014ddc1a7fe6a7fe8",
+    "url": "/static/media/bussola.8eac531f.png"
+  },
+  {
+    "revision": "37f84db3dd869c499781b7a14e306401",
+    "url": "/static/media/heart.37f84db3.png"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "785b4453f035641b0206ad581c0a642f",
+    "url": "/static/media/restaurant.785b4453.png"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
