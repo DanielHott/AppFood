322252b589df48f4a04a22c6ddb00b7752dab158
Daniel Hott <daniel_hott@outlook.com>
2022-01-13 15:49:24 -0300

    Deploy no gh pages

---
+++ build/precache-manifest.b8f2a10bfe6e24f51d891e51b2e16b2d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e904506ccad908851123119cbd77515",
+    "url": "/AppFood/index.html"
+  },
+  {
+    "revision": "b1b1a6302a261222462f",
+    "url": "/AppFood/static/css/2.389ceec1.chunk.css"
+  },
+  {
+    "revision": "95b0bd42259eb58a9a16",
+    "url": "/AppFood/static/css/main.0c1ae31f.chunk.css"
+  },
+  {
+    "revision": "b1b1a6302a261222462f",
+    "url": "/AppFood/static/js/2.09be1839.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/AppFood/static/js/2.09be1839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b0bd42259eb58a9a16",
+    "url": "/AppFood/static/js/main.e5da37ac.chunk.js"
+  },
+  {
+    "revision": "3595a95cdcb95e9348dd",
+    "url": "/AppFood/static/js/runtime-main.57bba699.js"
+  },
+  {
+    "revision": "30e3e10bfc3dcee165c61cc84eedb4eb",
+    "url": "/AppFood/static/media/beer.30e3e10b.png"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/AppFood/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "8eac531f3cd6dc1014ddc1a7fe6a7fe8",
+    "url": "/AppFood/static/media/bussola.8eac531f.png"
+  },
+  {
+    "revision": "37f84db3dd869c499781b7a14e306401",
+    "url": "/AppFood/static/media/heart.37f84db3.png"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/AppFood/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "785b4453f035641b0206ad581c0a642f",
+    "url": "/AppFood/static/media/restaurant.785b4453.png"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/AppFood/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/AppFood/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
